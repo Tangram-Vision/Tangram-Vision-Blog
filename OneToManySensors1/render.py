1ef38a06095508e2ef27e3a9e3188164c68d4fad
Brandon Minor <gallimatrix@gmail.com>
2020-12-02 15:54:24 -0600

    get rid of comments

--- OneToManySensors1/render.py
+++ OneToManySensors1/render.py
@@ -1,44 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Welcome to Jupyter!
-
-# This repo contains an introduction to [Jupyter](https://jupyter.org) and [IPython](https://ipython.org).
-# 
-# Outline of some basics:
-# 
-# * [Notebook Basics](../examples/Notebook/Notebook%20Basics.ipynb)
-# * [IPython - beyond plain python](../examples/IPython%20Kernel/Beyond%20Plain%20Python.ipynb)
-# * [Markdown Cells](../examples/Notebook/Working%20With%20Markdown%20Cells.ipynb)
-# * [Rich Display System](../examples/IPython%20Kernel/Rich%20Output.ipynb)
-# * [Custom Display logic](../examples/IPython%20Kernel/Custom%20Display%20Logic.ipynb)
-# * [Running a Secure Public Notebook Server](../examples/Notebook/Running%20the%20Notebook%20Server.ipynb#Securing-the-notebook-server)
-# * [How Jupyter works](../examples/Notebook/Multiple%20Languages%2C%20Frontends.ipynb) to run code in different languages.
-
-# You can also get this tutorial and run it on your laptop:
-# 
-#     git clone https://github.com/ipython/ipython-in-depth
-# 
-# Install IPython and Jupyter:
-# 
-# with [conda](https://www.anaconda.com/download):
-# 
-#     conda install ipython jupyter
-# 
-# with pip:
-# 
-#     # first, always upgrade pip!
-#     pip install --upgrade pip
-#     pip install --upgrade ipython jupyter
-# 
-# Start the notebook in the tutorial directory:
-# 
-#     cd ipython-in-depth
-#     jupyter notebook
-
-# In[144]:
-
-
 # import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
@@ -228,6 +190,3 @@ Z_final = multivariate_gaussian(pos, np.transpose(mu_final), Sigma_final)
 (ax, ax2) = plot_gaussian(X, Y, Z2, red_cmap)
 (ax, ax2) = add_gaussian(X, Y, Z_final, ax, ax2, blu_cmap)
 plt.show()
-
-
-
