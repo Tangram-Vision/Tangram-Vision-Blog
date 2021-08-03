970216314ade855773e21b2b0fd07b6944f20113
Jeremy Steward <jeremy.steward@tangramvision.com>
2021-08-03 11:56:16 -0600

    Format existing py files with "black" formatter

--- 2020.11.30_OneToManySensors/oneToManySensors.py
+++ 2020.11.30_OneToManySensors/oneToManySensors.py
@@ -11,37 +11,40 @@ from matplotlib.colors import ListedColormap
 import numpy as np
 from scipy.stats import multivariate_normal
 
+
 def multivariate_gaussian(X, Y, mu, Sigma):
     pos = np.empty(X.shape + (2,))
     pos[:, :, 0] = X
     pos[:, :, 1] = Y
     return multivariate_normal(mu, Sigma).pdf(pos)
 
+
 def plot_gaussian(X1, Y1, mu, Sigma, cmap):
     # Create a surface plot and projected filled contour plot under it.
     Z1 = multivariate_gaussian(X1, Y1, mu.reshape(2), Sigma)
 
-    fig = plt.figure(figsize=(16,8))
-    gs = gridspec.GridSpec(1, 2, width_ratios=[2,2])
-    ax = fig.add_subplot(gs[0], projection='3d')
-    ax.plot_surface(X1, Y1, Z1, rstride=3, cstride=3, linewidth=1, antialiased=True,
-                cmap=cmap)
-    max_range = np.array([X1.max()-X1.min(), Y1.max()-Y1.min(), Z1.max()-Z1.min()]).max() / 2.0
-    mid_x = (X1.max()+X1.min()) * 0.5
-    mid_y = (Y1.max()+Y1.min()) * 0.5
-    mid_z = (Z1.max()+Z1.min()) * 0.5
+    fig = plt.figure(figsize=(16, 8))
+    gs = gridspec.GridSpec(1, 2, width_ratios=[2, 2])
+    ax = fig.add_subplot(gs[0], projection="3d")
+    ax.plot_surface(X1, Y1, Z1, rstride=3, cstride=3, linewidth=1, antialiased=True, cmap=cmap)
+    max_range = (
+        np.array([X1.max() - X1.min(), Y1.max() - Y1.min(), Z1.max() - Z1.min()]).max() / 2.0
+    )
+    mid_x = (X1.max() + X1.min()) * 0.5
+    mid_y = (Y1.max() + Y1.min()) * 0.5
+    mid_z = (Z1.max() + Z1.min()) * 0.5
     ax.set_xlim(mid_x - max_range, mid_x + max_range)
     ax.set_ylim(mid_y - max_range, mid_y + max_range)
-    ax.set_xlabel('position')
-    ax.set_ylabel('velocity')
+    ax.set_xlabel("position")
+    ax.set_ylabel("velocity")
     ax.view_init(25, 290)
 
     ax2 = fig.add_subplot(gs[1])
     cset2 = ax2.contourf(X1, Y1, Z1, cmap=cmap)
-    ax2.axis('equal')
-    ax2.grid('true')
-    ax2.set_xlabel('position')
-    ax2.set_ylabel('velocity')
+    ax2.axis("equal")
+    ax2.grid("true")
+    ax2.set_xlabel("position")
+    ax2.set_ylabel("velocity")
     plt.tight_layout()
     return (ax, ax2)
 
@@ -50,36 +53,36 @@ def add_gaussian(X1, Y1, mu, Sigma, ax, ax2, cmap):
     # Create a surface plot and projected filled contour plot under it.
     Z1 = multivariate_gaussian(X1, Y1, mu.reshape(2), Sigma)
 
-    ax.plot_surface(X1, Y1, Z1, rstride=3, cstride=3, linewidth=1, antialiased=True,
-                cmap=cmap)
-    max_range = np.array([X1.max()-X1.min(), Y1.max()-Y1.min(), Z1.max()-Z1.min()]).max() / 2.0
+    ax.plot_surface(X1, Y1, Z1, rstride=3, cstride=3, linewidth=1, antialiased=True, cmap=cmap)
+    max_range = (
+        np.array([X1.max() - X1.min(), Y1.max() - Y1.min(), Z1.max() - Z1.min()]).max() / 2.0
+    )
 
-    mid_x = (X1.max()+X1.min()) * 0.5
-    mid_y = (Y1.max()+Y1.min()) * 0.5
-    mid_z = (Z1.max()+Z1.min()) * 0.5
+    mid_x = (X1.max() + X1.min()) * 0.5
+    mid_y = (Y1.max() + Y1.min()) * 0.5
+    mid_z = (Z1.max() + Z1.min()) * 0.5
     ax.set_xlim(mid_x - max_range, mid_x + max_range)
     ax.set_ylim(mid_y - max_range, mid_y + max_range)
     ax.view_init(25, 290)
     cset2 = ax2.contourf(X1, Y1, Z1, cmap=cmap)
-    ax2.axis('equal')
-    ax2.grid('true')
+    ax2.axis("equal")
+    ax2.grid("true")
     return (ax, ax2)
 
 
 def colormap(cmap, alpha_min, alpha_max):
     colormap = cmap(np.arange(cmap.N))
-    colormap[:,-1] = np.linspace(alpha_min, alpha_max, cmap.N)
+    colormap[:, -1] = np.linspace(alpha_min, alpha_max, cmap.N)
     return ListedColormap(colormap)
+
 
 def predict(delta_t, mu, Sigma):
     # Now let's add in our prediction matrix:
-    F = np.array([[1.0, delta_t],
-                  [0.0,     1.0]])
+    F = np.array([[1.0, delta_t], [0.0, 1.0]])
     # ...and our control matrix:
-    B = np.array([[(delta_t**2)/2], [delta_t]])
+    B = np.array([[(delta_t ** 2) / 2], [delta_t]])
     u = 2
-    Q = np.array([[0.2 , 0.0],
-                  [0.0,  0.4]])
+    Q = np.array([[0.2, 0.0], [0.0, 0.4]])
     mu_pred = F @ mu + B * u
     Sigma_pred = F @ Sigma @ F.T + Q
     return (mu_pred, Sigma_pred)
@@ -92,17 +95,14 @@ def main():
 
     # Mean vector and covariance matrix
     mu = np.array([[2.0], [2.0]])
-    Sigma = np.array([[0.8, 0.0],
-                      [0.0, 0.2]])
+    Sigma = np.array([[0.8, 0.0], [0.0, 0.2]])
 
     plot_gaussian(X, Y, mu, Sigma, colormap(pl.cm.Greens, 0.2, 1.0))
 
     ##################################
     # Showing variance vs covariance
-    Sigma_var = np.array([[0.5, 0.4],
-                          [0.1, 0.8]])
+    Sigma_var = np.array([[0.5, 0.4], [0.1, 0.8]])
     plot_gaussian(X, Y, mu, Sigma_var, colormap(pl.cm.Greens, 0.2, 1.0))
-
 
     ##################################
     # Prediction
@@ -118,8 +118,7 @@ def main():
 
     # We're only taking positional data, so let's use an observation
     # matrix that only selects for position:
-    H = np.array([[3.28084, 0.0],
-                  [0.0, 3.28084]])
+    H = np.array([[3.28084, 0.0], [0.0, 3.28084]])
 
     # Here, mu and Sigma in the measurement space are the same
     # However, this might not hold in other cases. Best to graph anyway
@@ -128,14 +127,24 @@ def main():
     (ax, ax2) = plot_gaussian(X, Y, mu_state_meas, Sigma_state_meas, colormap(pl.cm.Reds, 0.0, 0.9))
     (ax, ax2) = add_gaussian(X, Y, mu_pred, Sigma_pred, ax, ax2, colormap(pl.cm.Reds, 0.0, 0.5))
     delta = mu_state_meas - mu_pred
-    ax2.arrow(mu_pred[0, 0], mu_pred[1, 0], delta[0, 0], delta[1, 0], linestyle='-.', length_includes_head=True, head_width=1.0, head_length=1.0, fc='k', ec='k')
+    ax2.arrow(
+        mu_pred[0, 0],
+        mu_pred[1, 0],
+        delta[0, 0],
+        delta[1, 0],
+        linestyle="-.",
+        length_includes_head=True,
+        head_width=1.0,
+        head_length=1.0,
+        fc="k",
+        ec="k",
+    )
 
     ##################################
     # Update - Incorporating measurements
 
     z = mu_state_meas + np.array([[0.1], [-0.3]])
-    R_t = np.array([[3.0, 0.3],
-                    [0.0, 1.0]])
+    R_t = np.array([[3.0, 0.3], [0.0, 1.0]])
 
     (ax, ax2) = plot_gaussian(X, Y, mu_state_meas, Sigma_state_meas, colormap(pl.cm.Reds, 0.0, 0.9))
     (ax, ax2) = add_gaussian(X, Y, z, R_t, ax, ax2, colormap(pl.cm.YlGnBu, 0.0, 0.9))
@@ -155,7 +164,7 @@ def main():
 
     # and so:
     mu_add = K @ (z - (H @ mu_pred))
-    mu_final = (mu_pred + mu_add)
+    mu_final = mu_pred + mu_add
 
     # Factor out the Sigma_pred from each side, and you get:
     Sigma_fac = np.identity(2) - (K @ H)
--- 2021.01.21_CoordinateFrames/CoordinateFrames.py
+++ 2021.01.21_CoordinateFrames/CoordinateFrames.py
@@ -5,6 +5,7 @@ import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class WorldFramePlotter:
     def __init__(self, title, origin_name, xlim, ylim, text_offset_scale):
         self.text_offset_scale = text_offset_scale
@@ -32,7 +33,9 @@ class WorldFramePlotter:
         # Set up y-axis
         self.ax.set_ylim(ylim)
         self.ax.set_yticks([])
-        self.ax.set_ylabel(r"$y_{}$  ".format(origin_name), loc="top", rotation="horizontal", fontsize=22)
+        self.ax.set_ylabel(
+            r"$y_{}$  ".format(origin_name), loc="top", rotation="horizontal", fontsize=22
+        )
 
         ycolor = self.colormap[0]
         self.ax.spines["left"].set_color(ycolor)
@@ -41,28 +44,32 @@ class WorldFramePlotter:
         self.ax.plot(xlim[0], ylim[1], "^", ms=16, color=ycolor, clip_on=False)
 
         # Plot origin
-        self.ax.plot(0, 0, 'ko', zorder=1000, clip_on=False)
-        self.ax.text(-1 * 0.04 * xlim[1], -1 * 0.04 * ylim[1], r"$O_{}$".format(origin_name), fontsize=22)
+        self.ax.plot(0, 0, "ko", zorder=1000, clip_on=False)
+        self.ax.text(
+            -1 * 0.04 * xlim[1], -1 * 0.04 * ylim[1], r"$O_{}$".format(origin_name), fontsize=22
+        )
 
     def print_offset(self, limits):
         (lower, upper) = limits
         return self.text_offset_scale * (upper - lower)
 
     def add_labeled_point(self, x, y, label):
-        self.ax.plot(x, y, 'ko')
-        self.ax.text(x + self.print_offset(self.ax.get_xlim()),
-                     y + self.print_offset(self.ax.get_ylim()),
-                     f"${label}: ({x}, {y})$", fontsize=18)
+        self.ax.plot(x, y, "ko")
+        self.ax.text(
+            x + self.print_offset(self.ax.get_xlim()),
+            y + self.print_offset(self.ax.get_ylim()),
+            f"${label}: ({x}, {y})$",
+            fontsize=18,
+        )
 
     def add_grid_lines_for_point(self, x, y):
-        self.ax.plot([0, x], [y, y], '--', lw=1.0, color="black", alpha=0.3)
-        self.ax.plot([x, x], [0, y], '--', lw=1.0, color="black", alpha=0.3)
+        self.ax.plot([0, x], [y, y], "--", lw=1.0, color="black", alpha=0.3)
+        self.ax.plot([x, x], [0, y], "--", lw=1.0, color="black", alpha=0.3)
 
     def add_coordinate_frame(self, label, rotation, translation, scale, colors, label_origin=True):
         (x, y) = translation
         rot_rad = rotation * np.pi / 180
-        R = np.array([[np.cos(rot_rad), -np.sin(rot_rad)],
-                      [np.sin(rot_rad),  np.cos(rot_rad)]])
+        R = np.array([[np.cos(rot_rad), -np.sin(rot_rad)], [np.sin(rot_rad), np.cos(rot_rad)]])
 
         (xx, xy) = scale * R @ np.array([1, 0])
         (yx, yy) = scale * R @ np.array([0, 1])
@@ -70,24 +77,31 @@ class WorldFramePlotter:
         self.ax.arrow(x, y, xx, xy, width=0.3, color=colors[0])
         self.ax.arrow(x, y, yx, yy, width=0.3, color=colors[1])
 
-        self.ax.plot(x, y, 'ko', zorder=1000, clip_on=False)
+        self.ax.plot(x, y, "ko", zorder=1000, clip_on=False)
         if label_origin:
-            self.ax.text(x - self.print_offset(self.ax.get_xlim()),
-                         y - self.print_offset(self.ax.get_ylim()),
-                         r"$O_{}$".format(label),
-                         fontsize=22)
+            self.ax.text(
+                x - self.print_offset(self.ax.get_xlim()),
+                y - self.print_offset(self.ax.get_ylim()),
+                r"$O_{}$".format(label),
+                fontsize=22,
+            )
 
-        self.ax.text(x + xx + self.print_offset(self.ax.get_xlim()) / 2,
-                     y + xy,
-                     r"$x_{}$".format(label),
-                     fontsize=16)
-        self.ax.text(x + yx,
-                     y + yy + self.print_offset(self.ax.get_ylim()) / 2,
-                     r"$y_{}$".format(label),
-                     fontsize=16)
+        self.ax.text(
+            x + xx + self.print_offset(self.ax.get_xlim()) / 2,
+            y + xy,
+            r"$x_{}$".format(label),
+            fontsize=16,
+        )
+        self.ax.text(
+            x + yx,
+            y + yy + self.print_offset(self.ax.get_ylim()) / 2,
+            r"$y_{}$".format(label),
+            fontsize=16,
+        )
 
     def show(self):
         self.fig.show()
+
 
 def cartesian_grid():
     plot = WorldFramePlotter("Points on a Cartesian grid", " ", (0, 5), (0, 5), 0.04)
@@ -102,8 +116,11 @@ def cartesian_grid():
 
     plot.show()
 
+
 def world_frame_1():
-    plot = WorldFramePlotter("World frame with constituent local frames", "W", (0, 100), (0, 100), 0.04)
+    plot = WorldFramePlotter(
+        "World frame with constituent local frames", "W", (0, 100), (0, 100), 0.04
+    )
     frame1_colors = (plot.colormap[3], plot.colormap[2])
     plot.add_coordinate_frame("A", 40, (25, 65), 20, frame1_colors)
 
@@ -114,9 +131,11 @@ def world_frame_1():
 
     plot.show()
 
+
 def world_frame_translation():
-    plot = WorldFramePlotter("Translation between coordinate frames A and B",
-            "W", (0, 100), (0, 100), 0.04)
+    plot = WorldFramePlotter(
+        "Translation between coordinate frames A and B", "W", (0, 100), (0, 100), 0.04
+    )
 
     frame1_colors = (plot.colormap[3], plot.colormap[2])
     origin_A = np.array((25, 65))
@@ -131,16 +150,17 @@ def world_frame_translation():
     (dx, dy) = origin_B - origin_A
 
     plot.ax.arrow(
-            origin_A[0],
-            origin_A[1],
-            dx - 2,
-            dy + 1,
-            ls=(0, (10, 10)),
-            lw=0.5,
-            head_width=2,
-            head_length=2,
-            color="black",
-            alpha=0.4)
+        origin_A[0],
+        origin_A[1],
+        dx - 2,
+        dy + 1,
+        ls=(0, (10, 10)),
+        lw=0.5,
+        head_width=2,
+        head_length=2,
+        color="black",
+        alpha=0.4,
+    )
 
     midpoint = coords.mean(axis=0)
     offset = plot.print_offset(plot.ax.get_ylim())
@@ -149,9 +169,11 @@ def world_frame_translation():
 
     plot.show()
 
+
 def world_frame_rotation():
-    plot = WorldFramePlotter("Rotation between coordinate frames A and B",
-            "W", (0, 100), (0, 100), 0.04)
+    plot = WorldFramePlotter(
+        "Rotation between coordinate frames A and B", "W", (0, 100), (0, 100), 0.04
+    )
 
     frame1_colors = (plot.colormap[3], plot.colormap[2])
     origin = np.array((50, 50))
@@ -160,26 +182,25 @@ def world_frame_rotation():
     frame2_colors = (plot.colormap[5], plot.colormap[4])
     plot.add_coordinate_frame("B", 33, origin, 20, frame2_colors, False)
 
-    arcx = mpatches.Arc(origin, 25, 25, theta2 = 33, ls=(0, (5, 5)), alpha=0.4)
+    arcx = mpatches.Arc(origin, 25, 25, theta2=33, ls=(0, (5, 5)), alpha=0.4)
     plot.ax.add_patch(arcx)
 
-    arcy = mpatches.Arc(origin, 25, 25, theta1 = 90, theta2 = 123, ls=(0, (5, 5)), alpha=0.4)
+    arcy = mpatches.Arc(origin, 25, 25, theta1=90, theta2=123, ls=(0, (5, 5)), alpha=0.4)
     plot.ax.add_patch(arcy)
 
     plot.ax.text(63, 53, r"$\theta$", fontsize=18, alpha=0.4)
     plot.ax.text(45, 63, r"$\theta$", fontsize=18, alpha=0.4)
 
     print_offset = plot.print_offset(plot.ax.get_xlim())
-    plot.ax.text(origin[0] - print_offset,
-                 origin[1] - 2.5 * print_offset,
-                 r"$O_B$",
-                 fontsize=22)
+    plot.ax.text(origin[0] - print_offset, origin[1] - 2.5 * print_offset, r"$O_B$", fontsize=22)
 
     plot.show()
 
+
 def world_frame_scale():
-    plot = WorldFramePlotter("Scale between coordinate frames A and B",
-            "W", (0, 100), (0, 100), 0.04)
+    plot = WorldFramePlotter(
+        "Scale between coordinate frames A and B", "W", (0, 100), (0, 100), 0.04
+    )
 
     frame1_colors = (plot.colormap[3], plot.colormap[2])
     origin1 = np.array((30, 50))
@@ -190,6 +211,7 @@ def world_frame_scale():
     plot.add_coordinate_frame("B", 0, origin2, 4, frame2_colors)
 
     plot.show()
+
 
 def transform_as_separate_operations(sx, sy, theta, tx, ty, x_A, y_A):
     """
@@ -216,13 +238,15 @@ def transform_as_separate_operations(sx, sy, theta, tx, ty, x_A, y_A):
 
     A tuple (x_B, y_B) representing the transformed point
     """
-    scale = np.array([[sx, 0],
-                      [0, sy]])
-
+    scale = np.array([[sx, 0], [0, sy]])
 
     theta_in_radians = theta / 180 * np.pi
-    rotation = np.array([[np.cos(theta_in_radians), -np.sin(theta_in_radians)],
-                         [np.sin(theta_in_radians),  np.cos(theta_in_radians)]])
+    rotation = np.array(
+        [
+            [np.cos(theta_in_radians), -np.sin(theta_in_radians)],
+            [np.sin(theta_in_radians), np.cos(theta_in_radians)],
+        ]
+    )
 
     translation = np.array([tx, ty])
 
@@ -231,6 +255,7 @@ def transform_as_separate_operations(sx, sy, theta, tx, ty, x_A, y_A):
     [x_B, y_B] = scale @ rotation @ p_A + translation
 
     return (x_B, y_B)
+
 
 def transform_as_gamma_matrix(sx, sy, theta, tx, ty, x_A, y_A):
     """
@@ -255,9 +280,13 @@ def transform_as_gamma_matrix(sx, sy, theta, tx, ty, x_A, y_A):
     """
     theta_in_radians = theta / 180 * np.pi
 
-    Gamma = np.array([[sx*np.cos(theta_in_radians),  -sx*np.sin(theta_in_radians),  tx],
-                      [sy*np.sin(theta_in_radians),   sy*np.cos(theta_in_radians),  ty],
-                      [                          0,                             0,   1]])
+    Gamma = np.array(
+        [
+            [sx * np.cos(theta_in_radians), -sx * np.sin(theta_in_radians), tx],
+            [sy * np.sin(theta_in_radians), sy * np.cos(theta_in_radians), ty],
+            [0, 0, 1],
+        ]
+    )
 
     p_A = np.array([x_A, y_A, 1])
 
