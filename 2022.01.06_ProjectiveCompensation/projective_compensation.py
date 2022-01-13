29a7ad3f7420ca2e20712e8190993bc1a3cb7c83
Jeremy Steward <jeremy.steward@tangramvision.com>
2022-01-13 13:42:00 -0700

    Add docstring and change q_initial to x_initial for LS solution

--- 2022.01.06_ProjectiveCompensation/projective_compensation.py
+++ 2022.01.06_ProjectiveCompensation/projective_compensation.py
@@ -108,15 +108,47 @@ def problem_with_extra_observations():
     fig.show()
 
 
-def least_squares_solution_to_problem(d, ps, q_initial):
-    j = ((ps[:, 0] - q_initial[0]) / np.linalg.norm(ps - q_initial, axis=1)).reshape((-1, 1))
+def least_squares_solution_to_problem(d, ps, x_initial):
+    """
+    Sets up the least squares problem described by the figures.
+
+    The problem is fundamentally a trilateration problem, so we can say that our base model is:
+
+        d = norm_2(q)
+
+    or in plain English: the distance measured is equal to the 2-norm between points q and p, for
+    any two points q and p. Our functional model is only solving for x so we have only set `norm_2`
+    to vary according to the `x` coordinate.
+
+    This produces a Jacobian of the form:
+
+        J = [ d/dx (norm_2) ]
+
+    and a gradient of the form:
+
+        G = [ norm_2(x_initial) - d ]
+
+    From there, we can use the Jacobian and Gradient to perform a linear least-squares solution to
+    our problem.
+
+    @param d - an array of all observations.
+    @param ps - an array of all the points that have observations to our point `q`. Must be the same
+           length as `d`
+    @param x_initial - the initial x coordinate of point `q` to use as the seed for the
+           least-squares adjustment.
+
+    @returns (x, r) - the x coordinate of q, and the residual vector sharing the same length as `d`
+             for each observation
+    """
+    q_initial = (x_initial, 0)
+    j = ((ps[:, 0] - x_initial) / np.linalg.norm(ps - q_initial, axis=1)).reshape((-1, 1))
     g = np.linalg.norm(ps - q_initial, axis=1) - d
 
     N = j.T @ j
     U = j.T @ g
 
     delta = np.linalg.solve(-N, U)
-    x = q_initial[0] - delta
+    x = x_initial - delta
     r = j @ delta + g
 
     return (x, r)
@@ -130,9 +162,9 @@ if __name__ == "__main__":
     ## Problem 1:
     d = np.array([1.12, 1.86])
     ps = np.array([(5, 0), (8, 0)])
-    q_initial = (6, 0)
+    x_initial = 6
 
-    (x, r) = least_squares_solution_to_problem(d, ps, q_initial)
+    (x, r) = least_squares_solution_to_problem(d, ps, x_initial)
 
     print(f"x: {x}")
     print(f"r: {r}")
@@ -141,9 +173,9 @@ if __name__ == "__main__":
     ## Problem 2 -- Problem 1 but with error in second point
     d = np.array([1.12, 1.86])
     ps = np.array([(5, 0), (8, 0.6)])
-    q_initial = (6, 0)
+    x_initial = 6
 
-    (x, r) = least_squares_solution_to_problem(d, ps, q_initial)
+    (x, r) = least_squares_solution_to_problem(d, ps, x_initial)
 
     print(f"x: {x}")
     print(f"r: {r}")
@@ -152,9 +184,9 @@ if __name__ == "__main__":
     ## Problem 3 -- Problem 2 but we add two more points & observations
     d = np.array([1.12, 1.86, 1.36, 1.02])
     ps = np.array([(5, 0), (8, 0.6), (5.5, 1.2), (6.13, -1)])
-    q_initial = (6, 0)
+    x_initial = 6
 
-    (x, r) = least_squares_solution_to_problem(d, ps, q_initial)
+    (x, r) = least_squares_solution_to_problem(d, ps, x_initial)
 
     print(f"x: {x}")
     print(f"r: {r}")
